27eedb5769d84a387b56bc8ab97e0542d7ab5591
Vitcholas2000 <vitor_hugo-cunha@estudante.sesisenai.org.br>
2025-10-12 19:35:11 -0300

    feat: criei uma branch para desenvolvimento, crie alguns dos cs para desenvolvimentos de database e interação direto pelo cmd.

--- AmoxarifadoConsole/obj/Debug/net9.0/AmoxarifadoConsole.AssemblyInfo.cs
+++ AmoxarifadoConsole/obj/Debug/net9.0/AmoxarifadoConsole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmoxarifadoConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6516835c9396b65b7ac944739d2b2e0d46545e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmoxarifadoConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmoxarifadoConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
